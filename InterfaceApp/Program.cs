79a053e88eb774ccfcf9380cb31ea6f56c38ab4b
Amorcito1 <Apelegeretheresa054@gmail.com>
2025-08-09 14:06:35 +0000

    Added InterfaceApp

---
+++ InterfaceApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances of Car and Bicycle
+        IMovable myCar = new Car();
+        IMovable myBicycle = new Bicycle();
+
+        // Call the Move() method on each instance
+        myCar.Move();      // Output: Car is moving
+        myBicycle.Move();  // Output: Bicycle is moving
+    }
+}
